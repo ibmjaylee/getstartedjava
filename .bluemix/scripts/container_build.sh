f51f125c8e0b886586ed9e5647fb4ea1d2775e8f
jaylee <jhlee@kr.ibm.com>
2019-11-13 10:05:07 +0900

    deployment1.yaml update

--- .bluemix/scripts/container_build.sh
+++ .bluemix/scripts/container_build.sh
@@ -6,7 +6,7 @@ echo "REGISTRY_URL=${REGISTRY_URL}"
 echo "REGISTRY_NAMESPACE=${REGISTRY_NAMESPACE}"
 echo "IMAGE_NAME=${IMAGE_NAME}"
 echo "BUILD_NUMBER=${BUILD_NUMBER}"
-echo "ARCHIVE_DIR=${ARCHIVE_DIR}"
+#echo "ARCHIVE_DIR=${ARCHIVE_DIR}"
 
 # also run 'env' command to find all available env variables
 # or learn more about the available environment variables at:
@@ -38,31 +38,4 @@ set +x
 ibmcloud cr image-inspect ${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${BUILD_NUMBER}
 
 export PIPELINE_IMAGE_URL="$REGISTRY_URL/$REGISTRY_NAMESPACE/$IMAGE_NAME:$BUILD_NUMBER"
-
-echo "=========================================================="
-echo "COPYING ARTIFACTS needed for deployment and testing (in particular build.properties)"
-
-echo "Checking archive dir presence"
-mkdir -p $ARCHIVE_DIR
-
-# Persist env variables into a properties file (build.properties) so that all pipeline stages consuming this
-# build as input and configured with an environment properties file valued 'build.properties'
-# will be able to reuse the env variables in their job shell scripts.
-
-# IMAGE information from build.properties is used in Helm Chart deployment to set the release name
-echo "IMAGE_NAME=${IMAGE_NAME}" >> $ARCHIVE_DIR/build.properties
-echo "BUILD_NUMBER=${BUILD_NUMBER}" >> $ARCHIVE_DIR/build.properties
-# REGISTRY information from build.properties is used in Helm Chart deployment to generate cluster secret
-echo "REGISTRY_URL=${REGISTRY_URL}" >> $ARCHIVE_DIR/build.properties
-echo "REGISTRY_NAMESPACE=${REGISTRY_NAMESPACE}" >> $ARCHIVE_DIR/build.properties
-echo "File 'build.properties' created for passing env variables to subsequent pipeline jobs:"
-cat $ARCHIVE_DIR/build.properties
-
-echo "Copy pipeline scripts along with the build"
-# Copy scripts (incl. deploy scripts)
-if [ -d ./scripts/ ]; then
-  if [ ! -d $ARCHIVE_DIR/scripts/ ]; then # no need to copy if working in ./ already
-    cp -r ./scripts/ $ARCHIVE_DIR/
-  fi
-fi
 
